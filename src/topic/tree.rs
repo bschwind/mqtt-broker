a1318fba5f7db59eda3edbddb9152f3759537bd4
Brian Schwind <brianmschwind@gmail.com>
2020-01-24 22:24:56 +0900

    Fix SubscriptionTree::remove() to use TopicFilter type

--- src/topic/tree.rs
+++ src/topic/tree.rs
@@ -1,6 +1,6 @@
 use crate::topic::{
     filter::{TopicFilter, TopicLevel},
-    MULTI_LEVEL_WILDCARD_STR, SINGLE_LEVEL_WILDCARD_STR, TOPIC_SEPARATOR,
+    TOPIC_SEPARATOR,
 };
 use std::collections::{hash_map::Entry, HashMap};
 
@@ -25,7 +25,7 @@ impl<T: std::fmt::Debug> SubscriptionTree<T> {
         Self { root: SubscriptionTreeNode::new(), counter: 0 }
     }
 
-    pub fn insert(&mut self, topic_filter: TopicFilter, value: T) -> u64 {
+    pub fn insert(&mut self, topic_filter: &TopicFilter, value: T) -> u64 {
         let counter = self.counter;
         self.root.insert(topic_filter, value, counter);
         self.counter += 1;
@@ -37,7 +37,7 @@ impl<T: std::fmt::Debug> SubscriptionTree<T> {
         self.root.matching_subscribers(topic_name, sub_fn)
     }
 
-    pub fn remove(&mut self, topic_filter: String, counter: u64) -> Option<T> {
+    pub fn remove(&mut self, topic_filter: &TopicFilter, counter: u64) -> Option<T> {
         self.root.remove(topic_filter, counter)
     }
 
@@ -66,7 +66,7 @@ impl<T: std::fmt::Debug> SubscriptionTreeNode<T> {
             && self.concrete_topic_levels.is_empty();
     }
 
-    fn insert(&mut self, topic_filter: TopicFilter, value: T, counter: u64) {
+    fn insert(&mut self, topic_filter: &TopicFilter, value: T, counter: u64) {
         let mut current_tree = self;
         let mut multi_level = false;
 
@@ -113,16 +113,16 @@ impl<T: std::fmt::Debug> SubscriptionTreeNode<T> {
         }
     }
 
-    fn remove(&mut self, topic_filter: String, counter: u64) -> Option<T> {
+    fn remove(&mut self, topic_filter: &TopicFilter, counter: u64) -> Option<T> {
         let mut current_tree = self;
         let mut stack: Vec<(*mut SubscriptionTreeNode<T>, usize)> = vec![];
 
-        let levels: Vec<&str> = topic_filter.split(TOPIC_SEPARATOR).collect();
+        let levels: Vec<TopicLevel> = topic_filter.levels().collect();
         let mut level_index = 0;
 
         for level in &levels {
-            match *level {
-                SINGLE_LEVEL_WILDCARD_STR => {
+            match level {
+                TopicLevel::SingleLevelWildcard => {
                     if current_tree.single_level_wildcards.is_some() {
                         stack.push((&mut *current_tree, level_index));
                         level_index += 1;
@@ -132,17 +132,17 @@ impl<T: std::fmt::Debug> SubscriptionTreeNode<T> {
                         return None;
                     }
                 },
-                MULTI_LEVEL_WILDCARD_STR => {
+                TopicLevel::MultiLevelWildcard => {
                     break;
                 },
-                concrete_topic_level => {
-                    if current_tree.concrete_topic_levels.contains_key(concrete_topic_level) {
+                TopicLevel::Concrete(concrete_topic_level) => {
+                    if current_tree.concrete_topic_levels.contains_key(*concrete_topic_level) {
                         stack.push((&mut *current_tree, level_index));
                         level_index += 1;
 
                         current_tree = current_tree
                             .concrete_topic_levels
-                            .get_mut(concrete_topic_level)
+                            .get_mut(*concrete_topic_level)
                             .unwrap();
                     } else {
                         return None;
@@ -155,7 +155,7 @@ impl<T: std::fmt::Debug> SubscriptionTreeNode<T> {
         let return_val = {
             let level = &levels[levels.len() - 1];
 
-            if *level == MULTI_LEVEL_WILDCARD_STR {
+            if *level == TopicLevel::MultiLevelWildcard {
                 if let Some(pos) =
                     current_tree.multi_level_wildcards.iter().position(|(c, _)| *c == counter)
                 {
@@ -177,18 +177,18 @@ impl<T: std::fmt::Debug> SubscriptionTreeNode<T> {
         while let Some((stack_val, level_index)) = stack.pop() {
             let mut tree = unsafe { &mut *stack_val };
 
-            let level = levels[level_index];
+            let level = &levels[level_index];
 
             match level {
-                SINGLE_LEVEL_WILDCARD_STR => {
+                TopicLevel::SingleLevelWildcard => {
                     if tree.single_level_wildcards.as_ref().map(|t| t.is_empty()).unwrap_or(false) {
                         tree.single_level_wildcards = None;
                     }
                 },
-                MULTI_LEVEL_WILDCARD_STR => {
+                TopicLevel::MultiLevelWildcard => {
                     // TODO - Ignore this case?
                 },
-                concrete_topic_level => {
+                TopicLevel::Concrete(concrete_topic_level) => {
                     if let Entry::Occupied(o) =
                         tree.concrete_topic_levels.entry(concrete_topic_level.to_string())
                     {
@@ -250,19 +250,19 @@ mod tests {
     #[test]
     fn test_insert() {
         let mut sub_tree = SubscriptionTree::new();
-        sub_tree.insert("home/kitchen/temperature".parse().unwrap(), 1);
-        sub_tree.insert("home/kitchen/humidity".parse().unwrap(), 2);
-        sub_tree.insert("home/kitchen".parse().unwrap(), 3);
-        sub_tree.insert("home/+/humidity".parse().unwrap(), 4);
-        sub_tree.insert("home/+".parse().unwrap(), 5);
-        sub_tree.insert("home/#".parse().unwrap(), 6);
-        sub_tree.insert("home/+/temperature".parse().unwrap(), 7);
-        sub_tree.insert("office/stairwell/temperature".parse().unwrap(), 8);
-        sub_tree.insert("office/+/+".parse().unwrap(), 9);
-        sub_tree.insert("office/+/+/some_desk/+/fan_speed/+/temperature".parse().unwrap(), 10);
-        sub_tree.insert("office/+/+/some_desk/+/#".parse().unwrap(), 11);
-        sub_tree.insert("sport/tennis/+".parse().unwrap(), 21);
-        sub_tree.insert("#".parse().unwrap(), 12);
+        sub_tree.insert(&"home/kitchen/temperature".parse().unwrap(), 1);
+        sub_tree.insert(&"home/kitchen/humidity".parse().unwrap(), 2);
+        sub_tree.insert(&"home/kitchen".parse().unwrap(), 3);
+        sub_tree.insert(&"home/+/humidity".parse().unwrap(), 4);
+        sub_tree.insert(&"home/+".parse().unwrap(), 5);
+        sub_tree.insert(&"home/#".parse().unwrap(), 6);
+        sub_tree.insert(&"home/+/temperature".parse().unwrap(), 7);
+        sub_tree.insert(&"office/stairwell/temperature".parse().unwrap(), 8);
+        sub_tree.insert(&"office/+/+".parse().unwrap(), 9);
+        sub_tree.insert(&"office/+/+/some_desk/+/fan_speed/+/temperature".parse().unwrap(), 10);
+        sub_tree.insert(&"office/+/+/some_desk/+/#".parse().unwrap(), 11);
+        sub_tree.insert(&"sport/tennis/+".parse().unwrap(), 21);
+        sub_tree.insert(&"#".parse().unwrap(), 12);
 
         println!("{:#?}", sub_tree);
 
@@ -319,28 +319,37 @@ mod tests {
     #[test]
     fn test_remove() {
         let mut sub_tree = SubscriptionTree::new();
-        let sub_1 = sub_tree.insert("home/kitchen/temperature".parse().unwrap(), "sub_1");
-        let sub_2 = sub_tree.insert("home/kitchen/temperature".parse().unwrap(), "sub_2");
-        let sub_3 = sub_tree.insert("home/kitchen/humidity".parse().unwrap(), "sub_3");
-        let sub_4 = sub_tree.insert("home/kitchen/#".parse().unwrap(), "sub_4");
-        let sub_5 = sub_tree.insert("home/kitchen/+".parse().unwrap(), "sub_5");
-        let sub_6 = sub_tree.insert("home/kitchen/+".parse().unwrap(), "sub_6");
-        let sub_7 = sub_tree.insert("#".parse().unwrap(), "sub_7");
+        let sub_1 = sub_tree.insert(&"home/kitchen/temperature".parse().unwrap(), "sub_1");
+        let sub_2 = sub_tree.insert(&"home/kitchen/temperature".parse().unwrap(), "sub_2");
+        let sub_3 = sub_tree.insert(&"home/kitchen/humidity".parse().unwrap(), "sub_3");
+        let sub_4 = sub_tree.insert(&"home/kitchen/#".parse().unwrap(), "sub_4");
+        let sub_5 = sub_tree.insert(&"home/kitchen/+".parse().unwrap(), "sub_5");
+        let sub_6 = sub_tree.insert(&"home/kitchen/+".parse().unwrap(), "sub_6");
+        let sub_7 = sub_tree.insert(&"#".parse().unwrap(), "sub_7");
 
         assert!(!sub_tree.is_empty());
 
-        assert!(sub_tree.remove("#".to_string(), sub_1).is_none());
+        assert!(sub_tree.remove(&"#".parse().unwrap(), sub_1).is_none());
 
-        assert_eq!(sub_tree.remove("home/kitchen/temperature".to_string(), sub_1).unwrap(), "sub_1");
-        assert_eq!(sub_tree.remove("home/kitchen/temperature".to_string(), sub_2).unwrap(), "sub_2");
-        assert_eq!(sub_tree.remove("home/kitchen/#".to_string(), sub_4).unwrap(), "sub_4");
-        assert_eq!(sub_tree.remove("home/kitchen/+".to_string(), sub_5).unwrap(), "sub_5");
-        assert_eq!(sub_tree.remove("home/kitchen/humidity".to_string(), sub_3).unwrap(), "sub_3");
-        assert_eq!(sub_tree.remove("#".to_string(), sub_7).unwrap(), "sub_7");
-        assert_eq!(sub_tree.remove("home/kitchen/+".to_string(), sub_6).unwrap(), "sub_6");
+        assert_eq!(
+            sub_tree.remove(&"home/kitchen/temperature".parse().unwrap(), sub_1).unwrap(),
+            "sub_1"
+        );
+        assert_eq!(
+            sub_tree.remove(&"home/kitchen/temperature".parse().unwrap(), sub_2).unwrap(),
+            "sub_2"
+        );
+        assert_eq!(sub_tree.remove(&"home/kitchen/#".parse().unwrap(), sub_4).unwrap(), "sub_4");
+        assert_eq!(sub_tree.remove(&"home/kitchen/+".parse().unwrap(), sub_5).unwrap(), "sub_5");
+        assert_eq!(
+            sub_tree.remove(&"home/kitchen/humidity".parse().unwrap(), sub_3).unwrap(),
+            "sub_3"
+        );
+        assert_eq!(sub_tree.remove(&"#".parse().unwrap(), sub_7).unwrap(), "sub_7");
+        assert_eq!(sub_tree.remove(&"home/kitchen/+".parse().unwrap(), sub_6).unwrap(), "sub_6");
 
         assert!(sub_tree.is_empty());
 
-        assert!(sub_tree.remove("home/kitchen/+".to_string(), sub_6).is_none());
+        assert!(sub_tree.remove(&"home/kitchen/+".parse().unwrap(), sub_6).is_none());
     }
 }
